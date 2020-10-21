868f5506da8cd5664e23e922738dc4621794baa0
Stephen Wimberly <stephen.wimberly0@gmail.com>
2020-10-21 21:19:40 +0800

    Stripe payment integration in react native

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { PaymentScreen } from './src/screens/PaymentScreen';
+
+
+
+
+export default function App() {
+  return (
+      <PaymentScreen />
+  );
+}
+
+ 
